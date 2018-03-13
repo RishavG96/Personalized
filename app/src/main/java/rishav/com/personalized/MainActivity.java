0e6c7a861202d70edef1abe5b8648e29d732bc69
Rishav Gupta <gupta.rishav2608@gmail.com>
2018-03-13 19:09:09 +0530

    Started app

---
+++ app/src/main/java/rishav/com/personalized/MainActivity.java
@@ -0,0 +1,13 @@
+package rishav.com.personalized;
+            
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
